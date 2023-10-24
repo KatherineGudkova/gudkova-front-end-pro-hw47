6a63ccef14cb34f7b2c198bee9a1a552936938a2
Gudkova Kateryna <kikigudkova@gmail.com>
2023-10-24 12:05:34 +0300

    program code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./App";
+import todoReducer from "./reducers";
+
+const store = createStore(todoReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
